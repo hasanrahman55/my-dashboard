d8c2f6b89a414b3a570d43fb508d6254ed0ef5f5
Hasan Rahman <hasanrahman33605@gmail.com>
2025-02-02 17:20:29 +0600

    ..

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
@@ -16,13 +17,17 @@ export default function RootLayout({
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 bg-[#fafafa] shadow-lg">
+          <header className="fixed w-full z-[999] flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 bg-[#fafafa] shadow-lg  dark:bg-[#1a1a1a] dark:text-white">
             <div className="flex items-center gap-2 px-4">
               <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 h-4 dark:bg-white"
+              />
+              <ModeToggle />
             </div>
           </header>
-          <div className="p-6">{children}</div>
+          <div className="p-6 mt-16">{children}</div>
         </SidebarInset>
       </SidebarProvider>
     </>
