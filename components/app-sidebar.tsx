2f09eafbb8af741e5325d45a6d30be031dd06c36
Hasan Rahman <hasanrahman33605@gmail.com>
2025-02-12 15:09:39 +0600

    ...

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -49,30 +49,6 @@ const data = {
         },
       ],
     },
-
-    // {
-    //   title: "Documentation",
-    //   url: "#",
-    //   icon: BookOpen,
-    //   items: [
-    //     {
-    //       title: "Introduction",
-    //       url: "#",
-    //     },
-    //     {
-    //       title: "Get Started",
-    //       url: "#",
-    //     },
-    //     {
-    //       title: "Tutorials",
-    //       url: "#",
-    //     },
-    //     {
-    //       title: "Changelog",
-    //       url: "#",
-    //     },
-    //   ],
-    // },
   ],
   projects: [
     {
